929df587780d10237704c4a85dcbe537c7dcda3e
AllanSechrist <sechristallan@gmail.com>
2022-10-17 20:39:17 +0900

    revoke and change secret

--- finance/settings.py
+++ finance/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from .secret import SECRET
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-9lj+^zlq76e^4hx$r32h%qhin=od9-k2u5xo1+5269zix%$b8c"
+SECRET_KEY = SECRET
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
